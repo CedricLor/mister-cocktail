a765dd2c4863118dfa7e954e7b793c0a3221d1a6
Cedric <cedric.lor@gmail.com>
2015-06-11 15:54:22 +0200

    migrations, models, validations, associations completed, controllers and views created, to be worked upon

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150611125201) do
+
+  create_table "cocktails", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "doses", force: :cascade do |t|
+    t.string   "description"
+    t.integer  "ingredient_id"
+    t.integer  "cocktail_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "doses", ["cocktail_id"], name: "index_doses_on_cocktail_id"
+  add_index "doses", ["ingredient_id"], name: "index_doses_on_ingredient_id"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Cocktail.create(name: "Mint Julep")
+Cocktail.create(name: "Whiskey Sour")
+Cocktail.create(name: "Mojito")
